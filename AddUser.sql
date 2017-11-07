d0cc6cd36b983a2b85e23a8abeea527c6ed2b846
Alessio Parma <alessio.parma@gmail.com>
2017-11-07 16:36:51 +0100

    Update AddUser.sql

--- AddUser.sql
+++ AddUser.sql
@@ -1,4 +1,4 @@
-define ORACLE_USER = '???'; -- Set here the other Oracle user who needs to send log entries to Seq
+define ORACLE_USER = '???'; -- Set here the other Oracle user who needs to send log events to Seq
 
 grant execute on utl_http to &ORACLE_USER;
 grant execute on dbms_lock to &ORACLE_USER;
