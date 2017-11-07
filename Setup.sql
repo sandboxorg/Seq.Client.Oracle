00c369a2aaf480b9ecd37e60729e0db97dbaacbe
Alessio Parma <alessio.parma@gmail.com>
2017-11-07 14:14:15 +0100

    Update Setup.sql

--- Setup.sql
+++ Setup.sql
@@ -1,25 +1,24 @@
-define ORACLE_USER  = '???';  -- Set here the Oracle user who needs to send log entries to Seq
-define SEQ_HOST     = '???';  -- Set here the host name (and path, if any) on which Seq is listening to
-define SEQ_PORT     = 5341;   -- Set here the port number on which Seq is listening to - Default is 5341
+define ORACLE_USER = '???'; -- Set here the Oracle user who needs to send log entries to Seq
+define SEQ_HOST    = '???'; -- Set here the host name on which Seq is listening to
+define SEQ_PORT    = 5341;  -- Set here the port number on which Seq is listening to - Default is 5341
 
 grant execute on utl_http to &ORACLE_USER;
 grant execute on dbms_lock to &ORACLE_USER;
 
 begin
-  DBMS_NETWORK_ACL_ADMIN.create_acl (
-    acl          => 'local_sx_acl_file.xml', 
-    description  => 'A test of the ACL functionality',
-    principal    => '&ORACLE_USER',
-    is_grant     => TRUE, 
-    privilege    => 'connect',
-    start_date   => SYSTIMESTAMP,
-    end_date     => NULL);
-end;
+  DBMS_NETWORK_ACL_ADMIN.create_acl(acl         => 'www.xml',
+                                    description => 'WWW ACL',
+                                    principal   => '&ORACLE_USER',
+                                    is_grant    => true,
+                                    privilege   => 'connect');
+  
+  DBMS_NETWORK_ACL_ADMIN.add_privilege(acl       => 'www.xml',
+                                       principal => '&ORACLE_USER',
+                                       is_grant  => true,
+                                       privilege => 'resolve');
 
-begin
-  DBMS_NETWORK_ACL_ADMIN.assign_acl (
-    acl         => 'local_sx_acl_file.xml',
-    host        => '&SEQ_HOST', 
-    lower_port  => &SEQ_PORT,
-    upper_port  => NULL);    
-end; 
+  DBMS_NETWORK_ACL_ADMIN.assign_acl (acl         => 'www.xml',
+                                     host        => '&SEQ_HOST',
+                                     lower_port  => &SEQ_PORT,
+                                     upper_port  => null);
+end;
