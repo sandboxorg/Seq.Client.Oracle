af01d18f6c399db48ba83a31f5803938c0ea5408
Alessio Parma <alessio.parma@gmail.com>
2017-11-07 11:45:55 +0100

    Update Setup.sql

--- Setup.sql
+++ Setup.sql
@@ -1,24 +1,24 @@
--- Replace $ORACLE_USER$ with the Oracle user who needs to send log messages to Seq.
+define ORACLE_USER  = '???';  -- Set here the Oracle user who needs to send log entries to Seq
+define SEQ_PORT     = 5341;   -- Set here the port number on which Seq is listening on - Default is 5341
 
+grant execute on utl_http to &ORACLE_USER;
+grant execute on dbms_lock to &ORACLE_USER;
 
-grant execute on utl_http to wc;
-grant execute on dbms_lock to wc;
- 
-BEGIN
+begin
   DBMS_NETWORK_ACL_ADMIN.create_acl (
     acl          => 'local_sx_acl_file.xml', 
     description  => 'A test of the ACL functionality',
-    principal    => 'WC',
+    principal    => '&ORACLE_USER',
     is_grant     => TRUE, 
     privilege    => 'connect',
     start_date   => SYSTIMESTAMP,
     end_date     => NULL);
 end;
- 
+
 begin
   DBMS_NETWORK_ACL_ADMIN.assign_acl (
     acl         => 'local_sx_acl_file.xml',
     host        => 'localhost', 
-    lower_port  => 9002,
+    lower_port  => 5341,
     upper_port  => NULL);    
 end; 
