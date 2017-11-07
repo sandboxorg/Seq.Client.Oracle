a1365d5701d671f807ecf47a23e0be6e3186bde7
Alessio Parma <alessio.parma@gmail.com>
2017-11-07 15:37:41 +0100

    Update Package.sql

--- Package.sql
+++ Package.sql
@@ -1,1 +1,3 @@
-
+define ORACLE_USER = '???'; -- Set here the Oracle user for whom package should be created
+define SEQ_HOST    = '???'; -- Set here the host name on which Seq is listening to
+define SEQ_PORT    = 5341;  -- Set here the port number on which Seq is listening to - Default is 5341
