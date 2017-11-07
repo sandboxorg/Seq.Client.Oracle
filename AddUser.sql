707f6996820c2f902e68d442f27cfbcca412c363
Alessio Parma <alessio.parma@gmail.com>
2017-11-07 15:27:21 +0100

    Update AddUser.sql

--- AddUser.sql
+++ AddUser.sql
@@ -1,1 +1,18 @@
+define ORACLE_USER = '???'; -- Set here the other Oracle user who needs to send log entries to Seq
 
+grant execute on utl_http to &ORACLE_USER;
+grant execute on dbms_lock to &ORACLE_USER;
+
+begin
+  DBMS_NETWORK_ACL_ADMIN.add_privilege(acl       => 'www.xml',
+                                       principal => '&ORACLE_USER',
+                                       is_grant  => true,
+                                       privilege => 'connect');
+  
+  DBMS_NETWORK_ACL_ADMIN.add_privilege(acl       => 'www.xml',
+                                       principal => '&ORACLE_USER',
+                                       is_grant  => true,
+                                       privilege => 'resolve');
+end;
+
+commit;
