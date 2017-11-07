d318640882f57d9de0df291f9b3721c84eb5e0ee
Alessio Parma <alessio.parma@gmail.com>
2017-11-07 12:40:15 +0100

    Update Setup.sql

--- Setup.sql
+++ Setup.sql
@@ -1,6 +1,6 @@
 define ORACLE_USER  = '???';  -- Set here the Oracle user who needs to send log entries to Seq
-define SEQ_HOST     = '???';  -- Set here the host name (and path, if any) on which Seq is listening
-define SEQ_PORT     = 5341;   -- Set here the port number on which Seq is listening - Default is 5341
+define SEQ_HOST     = '???';  -- Set here the host name (and path, if any) on which Seq is listening to
+define SEQ_PORT     = 5341;   -- Set here the port number on which Seq is listening to - Default is 5341
 
 grant execute on utl_http to &ORACLE_USER;
 grant execute on dbms_lock to &ORACLE_USER;
