66ac46bdbd4c827620c98eb93bc071658048d2b1
Alessio Parma <alessio.parma@gmail.com>
2017-11-09 17:20:54 +0100

    log package

--- Package.sql
+++ Package.sql
@@ -8,8 +8,8 @@ define DOT = '.'; -- Internal use only.
 
 create or replace package &ORACLE_USER&DOT&ORACLE_PACKAGE as
 
-  type evt_prop is record (name nvarchar2(100), 
-                           value nvarchar2(2000));
+  type evt_prop is record (name varchar2(100), 
+                           value varchar2(4000));
                              
   type evt_props is varray(20) of evt_prop;
 
@@ -21,29 +21,29 @@ create or replace package &ORACLE_USER&DOT&ORACLE_PACKAGE as
   
   function clef_template return varchar2 deterministic;
 
-  function json_escape(str in nvarchar2) return nvarchar2 deterministic;
+  function escape_json(str in varchar2) return varchar2 deterministic;
 
-  procedure verbose(message_template in nvarchar2,
+  procedure verbose(message_template in varchar2,
                     event_props in evt_props);
 
-  procedure debug(message_template in nvarchar2,
+  procedure debug(message_template in varchar2,
                   event_props in evt_props);
   
-  procedure information(message_template in nvarchar2,
+  procedure information(message_template in varchar2,
                         event_props in evt_props);
 
-  procedure warning(message_template in nvarchar2,
+  procedure warning(message_template in varchar2,
                     event_props in evt_props);
 
-  procedure error(message_template in nvarchar2,
+  procedure error(message_template in varchar2,
                   event_props in evt_props);
 
-  procedure fatal(message_template in nvarchar2,
+  procedure fatal(message_template in varchar2,
                   event_props in evt_props);
   
   procedure send_log_event(api_key in varchar2,
                            log_level in varchar2,
-                           message_template in nvarchar2,
+                           message_template in varchar2,
                            event_props in evt_props,
                            owner in varchar2,
                            program_unit in varchar2,
@@ -79,21 +79,21 @@ create or replace package body &ORACLE_USER&DOT&ORACLE_PACKAGE as
     return '{"@t":"[[timestamp]]","@l":"[[log_level]]","@mt":"[[message_template]]"[[event_props]]}';
   end clef_template;
   
-  function json_escape(str in nvarchar2) return nvarchar2 deterministic
+  function escape_json(str in varchar2) return varchar2 deterministic
   is
-    esc nvarchar2(2000);  
+    esc varchar2(32767);  
   begin
     -- Start replacing all backslashes, to avoid replacing propers escapes.
-    esc := regexp_replace(str, '' || chr(92), '\\');
+    esc := regexp_replace(str, '\\', '\\\');
     -- Then, continue with other reserved characters.
     esc := regexp_replace(esc, '' || chr(10), '\n');
     esc := regexp_replace(esc, '' || chr(13), '\r');
     esc := regexp_replace(esc, '' || chr(11), '\t');
     esc := regexp_replace(esc, '' || chr(34), '\"');
-    return esc;
-  end json_escape;
+    return '"' || esc || '"';
+  end escape_json;
   
-  procedure verbose(message_template in nvarchar2,
+  procedure verbose(message_template in varchar2,
                     event_props in evt_props)
   is
     owner varchar2(100);
@@ -105,7 +105,7 @@ create or replace package body &ORACLE_USER&DOT&ORACLE_PACKAGE as
     send_log_event(null, 'Verbose', message_template, event_props, owner, program_unit, line_number);
   end verbose;
 
-  procedure debug(message_template in nvarchar2,
+  procedure debug(message_template in varchar2,
                   event_props in evt_props)
   is
     owner varchar2(100);
@@ -117,7 +117,7 @@ create or replace package body &ORACLE_USER&DOT&ORACLE_PACKAGE as
     send_log_event(null, 'Debug', message_template, event_props, owner, program_unit, line_number);
   end debug;
 
-  procedure information(message_template in nvarchar2,
+  procedure information(message_template in varchar2,
                         event_props in evt_props)
   is
     owner varchar2(100);
@@ -129,7 +129,7 @@ create or replace package body &ORACLE_USER&DOT&ORACLE_PACKAGE as
     send_log_event(null, 'Information', message_template, event_props, owner, program_unit, line_number);
   end information;
 
-  procedure warning(message_template in nvarchar2,
+  procedure warning(message_template in varchar2,
                     event_props in evt_props)
   is
     owner varchar2(100);
@@ -141,7 +141,7 @@ create or replace package body &ORACLE_USER&DOT&ORACLE_PACKAGE as
     send_log_event(null, 'Warning', message_template, event_props, owner, program_unit, line_number);
   end warning;
 
-  procedure error(message_template in nvarchar2,
+  procedure error(message_template in varchar2,
                   event_props in evt_props)
   is
     owner varchar2(100);
@@ -153,7 +153,7 @@ create or replace package body &ORACLE_USER&DOT&ORACLE_PACKAGE as
     send_log_event(null, 'Error', message_template, event_props, owner, program_unit, line_number);
   end error;
 
-  procedure fatal(message_template in nvarchar2,
+  procedure fatal(message_template in varchar2,
                   event_props in evt_props)
   is
     owner varchar2(100);
@@ -167,7 +167,7 @@ create or replace package body &ORACLE_USER&DOT&ORACLE_PACKAGE as
   
   procedure send_log_event(api_key in varchar2,
                            log_level in varchar2,
-                           message_template in nvarchar2,
+                           message_template in varchar2,
                            event_props in evt_props,
                            owner in varchar2,
                            program_unit in varchar2,
@@ -176,19 +176,19 @@ create or replace package body &ORACLE_USER&DOT&ORACLE_PACKAGE as
     request utl_http.req;
     response utl_http.resp;
     timestamp varchar2(100);
-    event_props_json nvarchar2(2000);
-    log_event nvarchar2(2000);
-    buffer nvarchar2(2000);
+    event_props_json varchar2(32767);
+    log_event varchar2(32767);
+    buffer varchar2(32767);
   begin
     timestamp := to_char(systimestamp at time zone 'UTC', 'yyyy-mm-dd"T"hh24:mi:ss.ff3"Z"');
     
-    select listagg(',"' || json_escape(x.name) || '":"' || json_escape(x.value) || '"') within group (order by x.name)
+    select listagg(',' || escape_json(x.name) || ':' || escape_json(x.value)) within group (order by x.name)
       into event_props_json
-      from (select name, value from table(event_props)
+      from (select name, value from table(coalesce(event_props, evt_props()))
              union all
-            select to_nchar('SourceContext') name, to_nchar(upper(owner) || '.' || upper(program_unit)) value from dual
+            select 'SourceContext' name, (lower(owner) || '.' || lower(program_unit)) value from dual
              union all
-            select to_nchar('LineNumber') name, to_nchar(line_number) value from dual) x;
+            select 'LineNumber' name, to_char(line_number) value from dual) x;
   
     log_event := clef_template;
     log_event := replace(log_event, '[[timestamp]]', timestamp);
@@ -197,15 +197,26 @@ create or replace package body &ORACLE_USER&DOT&ORACLE_PACKAGE as
     log_event := replace(log_event, '[[event_props]]', event_props_json);
   
     request := utl_http.begin_request(raw_events_url, 'POST',' HTTP/1.1');
-    utl_http.set_header(request, 'User-Agent', 'Seq client for Oracle/PLSQL'); 
-    utl_http.set_header(request, 'Content-Type', 'application/json'); 
+    utl_http.set_header(request, 'User-Agent', 'Mozilla/4.0'); 
+    utl_http.set_header(request, 'Content-Type', 'application/json;charset=utf-8'); 
     utl_http.set_header(request, 'Content-Length', length(log_event));
     utl_http.set_header(request, 'Accept', 'application/json');
     utl_http.set_header(request, 'X-Seq-ApiKey', coalesce(api_key, default_api_key));
   
     utl_http.write_text(request, log_event);
     response := utl_http.get_response(request);
-    utl_http.end_response(response);    
+
+    -- Process the response from the HTTP call.
+    begin
+      loop
+        utl_http.read_line(response, buffer);
+        dbms_output.put_line(buffer);
+      end loop;
+      utl_http.end_response(response);
+    exception
+      when utl_http.end_of_body then
+        utl_http.end_response(response);
+    end;   
   exception
     when others then
       null; -- Do nothing when an error happens.
