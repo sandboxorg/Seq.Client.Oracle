bd598bda57be27114f11e9a79cb8da44ca7268ad
Alessio Parma <alessio.parma@gmail.com>
2017-11-09 10:39:40 +0100

    pkg

--- Package.sql
+++ Package.sql
@@ -6,20 +6,20 @@ define SEQ_DEFAULT_API_KEY = '???';     -- Set here the default API KEY which wi
 
 define DOT = '.'; -- Internal use only.
 
-create package &ORACLE_USER&DOT&ORACLE_PACKAGE as
+create or replace package &ORACLE_USER&DOT&ORACLE_PACKAGE as
 
   type evt_prop is record (name nvarchar2(100), 
                            value nvarchar2(2000));
                              
   type evt_props is varray(20) of evt_prop;
 
-  function seq_base_url return varchar2 deterministic;
+  function base_url return varchar2 deterministic;
   
-  function seq_raw_events_url return varchar2 deterministic;
+  function raw_events_url return varchar2 deterministic;
   
-  function seq_default_api_key return varchar2 deterministic;
+  function default_api_key return varchar2 deterministic;
   
-  function seq_clef_template return varchar2 deterministic;
+  function clef_template return varchar2 deterministic;
 
   function json_escape(str in nvarchar2) return nvarchar2 deterministic;
 
@@ -55,29 +55,29 @@ end &ORACLE_PACKAGE;
 /
 create or replace package body &ORACLE_USER&DOT&ORACLE_PACKAGE as
 
-  function seq_base_url return varchar2 deterministic
+  function base_url return varchar2 deterministic
   is
   begin
     return 'http://&SEQ_HOST:&SEQ_PORT/api/';
-  end seq_base_url;
+  end base_url;
   
-  function seq_raw_events_url return varchar2 deterministic
+  function raw_events_url return varchar2 deterministic
   is
   begin
-    return seq_base_url || 'events/raw?clef';
-  end seq_raw_events_url;
+    return base_url || 'events/raw?clef';
+  end raw_events_url;
   
-  function seq_default_api_key return varchar2 deterministic
+  function default_api_key return varchar2 deterministic
   is
   begin
     return '&SEQ_DEFAULT_API_KEY';
-  end seq_default_api_key;
+  end default_api_key;
   
-  function seq_clef_template return varchar2 deterministic
+  function clef_template return varchar2 deterministic
   is
   begin
     return '{"@t":"[[timestamp]]","@l":"[[log_level]]","@mt":"[[message_template]]"[[event_props]]}';
-  end seq_clef_template;
+  end clef_template;
   
   function json_escape(str in nvarchar2) return nvarchar2 deterministic
   is
@@ -97,72 +97,72 @@ create or replace package body &ORACLE_USER&DOT&ORACLE_PACKAGE as
                     event_props in evt_props)
   is
     owner varchar2(100);
-    program_unit varchar2;
-    line_number number;
-    caller_type varchar2;
+    program_unit varchar2(100);
+    line_number number(10);
+    caller_type varchar2(100);
   begin
     owa_util.who_called_me(owner, program_unit, line_number, caller_type);
     send_log_event(null, 'Verbose', message_template, event_props, owner, program_unit, line_number);
   end verbose;
-  
+
   procedure debug(message_template in nvarchar2,
                   event_props in evt_props)
   is
-    owner varchar2;
-    program_unit varchar2;
-    line_number number;
-    caller_type varchar2;
+    owner varchar2(100);
+    program_unit varchar2(100);
+    line_number number(10);
+    caller_type varchar2(100);
   begin
     owa_util.who_called_me(owner, program_unit, line_number, caller_type);
-    send_log_event(null, 'Debug', message_template, event_props, program_unit, line_number);
+    send_log_event(null, 'Debug', message_template, event_props, owner, program_unit, line_number);
   end debug;
-  
+
   procedure information(message_template in nvarchar2,
                         event_props in evt_props)
   is
-    owner varchar2;
-    program_unit varchar2;
-    line_number number;
-    caller_type varchar2;
+    owner varchar2(100);
+    program_unit varchar2(100);
+    line_number number(10);
+    caller_type varchar2(100);
   begin
     owa_util.who_called_me(owner, program_unit, line_number, caller_type);
-    send_log_event(null, 'Information', message_template, event_props, program_unit, line_number);
+    send_log_event(null, 'Information', message_template, event_props, owner, program_unit, line_number);
   end information;
-  
+
   procedure warning(message_template in nvarchar2,
                     event_props in evt_props)
   is
-    owner varchar2;
-    program_unit varchar2;
-    line_number number;
-    caller_type varchar2;
+    owner varchar2(100);
+    program_unit varchar2(100);
+    line_number number(10);
+    caller_type varchar2(100);
   begin
     owa_util.who_called_me(owner, program_unit, line_number, caller_type);
-    send_log_event(null, 'Warning', message_template, event_props, program_unit, line_number);
+    send_log_event(null, 'Warning', message_template, event_props, owner, program_unit, line_number);
   end warning;
-  
+
   procedure error(message_template in nvarchar2,
                   event_props in evt_props)
   is
-    owner varchar2;
-    program_unit varchar2;
-    line_number number;
-    caller_type varchar2;
+    owner varchar2(100);
+    program_unit varchar2(100);
+    line_number number(10);
+    caller_type varchar2(100);
   begin
     owa_util.who_called_me(owner, program_unit, line_number, caller_type);
-    send_log_event(null, 'Error', message_template, event_props, program_unit, line_number);
+    send_log_event(null, 'Error', message_template, event_props, owner, program_unit, line_number);
   end error;
-  
+
   procedure fatal(message_template in nvarchar2,
                   event_props in evt_props)
   is
-    owner varchar2;
-    program_unit varchar2;
-    line_number number;
-    caller_type varchar2;
+    owner varchar2(100);
+    program_unit varchar2(100);
+    line_number number(10);
+    caller_type varchar2(100);
   begin
     owa_util.who_called_me(owner, program_unit, line_number, caller_type);
-    send_log_event(null, 'Fatal', message_template, event_props, program_unit, line_number);
+    send_log_event(null, 'Fatal', message_template, event_props, owner, program_unit, line_number);
   end fatal;
   
   procedure send_log_event(api_key in varchar2,
@@ -175,34 +175,33 @@ create or replace package body &ORACLE_USER&DOT&ORACLE_PACKAGE as
   is
     request utl_http.req;
     response utl_http.resp;
-    timestamp varchar2;
-    event_props_json nvarchar2;
-    log_event nvarchar2;
-    buffer nvarchar2;
+    timestamp varchar2(100);
+    event_props_json nvarchar2(2000);
+    log_event nvarchar2(2000);
+    buffer nvarchar2(2000);
   begin
-    api_key   := coalesce(api_key, seq_default_api_key());
     timestamp := to_char(systimestamp at time zone 'UTC', 'yyyy-mm-dd"T"hh24:mi:ss.ff3"Z"');
     
-    select listagg(',"' || json_escape(x.key) || '":"' || json_escape(x.value) || '"') within group (order by x.key)
+    select listagg(',"' || json_escape(x.name) || '":"' || json_escape(x.value) || '"') within group (order by x.name)
       into event_props_json
-      from (select key, value from table(event_props)
+      from (select name, value from table(event_props)
              union all
-            select 'SourceContext' key, (upper(owner) || '.' || upper(program_unit)) value from dual
+            select to_nchar('SourceContext') name, to_nchar(upper(owner) || '.' || upper(program_unit)) value from dual
              union all
-            select 'LineNumber' key, to_char(line_number) value from dual) x;
+            select to_nchar('LineNumber') name, to_nchar(line_number) value from dual) x;
   
-    log_event := seq_clef_template();
+    log_event := clef_template;
     log_event := replace(log_event, '[[timestamp]]', timestamp);
     log_event := replace(log_event, '[[log_level]]', log_level);
     log_event := replace(log_event, '[[message_template]]', message_template);
     log_event := replace(log_event, '[[event_props]]', event_props_json);
   
-    request := utl_http.begin_request(seq_raw_events_url(), 'POST',' HTTP/1.1');
+    request := utl_http.begin_request(raw_events_url, 'POST',' HTTP/1.1');
     utl_http.set_header(request, 'User-Agent', 'Seq client for Oracle/PLSQL'); 
     utl_http.set_header(request, 'Content-Type', 'application/json'); 
     utl_http.set_header(request, 'Content-Length', length(log_event));
     utl_http.set_header(request, 'Accept', 'application/json');
-    utl_http.set_header(request, 'X-Seq-ApiKey', api_key);
+    utl_http.set_header(request, 'X-Seq-ApiKey', coalesce(api_key, default_api_key));
   
     utl_http.write_text(request, log_event);
     response := utl_http.get_response(request);
